9195fcdc612ae6d01e48ecbccf3f02099f8e7ed1
SGS Sandhu <sgs.sandhu@gmail.com>
2017-12-15 12:37:37 +0530

    Update 20171215070737

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
-	$('#aione_header').load('template/header.html');
+	$('#aione_header').load('template/header.html', function() {
+		var options = {
+			valueNames: ['aione-search-item'],
+			searchClass: 'aione-search-input',
+			listClass: 'aione-search-list'
+		};
+		var search = new List('aione_nav', options);
+	});
 	$('#aione_footer').load('template/footer.html');
 	$('#aione_copyright').load('template/copyright.html');
 });
