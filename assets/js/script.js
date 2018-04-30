9677358fffb02838fcee17cd46caf3f946206582
SGS Sandhu <sgs.sandhu@gmail.com>
2018-04-30 18:02:10 +0530

    Update 20180430123210

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
+	
+    $(".options").click();
+    $(".options").click(function(e) {
+        var options = "";
+        var options_data = $(this).find("form").serializeArray();
+        jQuery.each( options_data, function( index, field ) {
+          options += " " + field.value;
+        });
+        $( "#tab_preview > *").attr('class',options);
+        $( "#tab_code > textarea").val($( "#tab_preview > *")[0].outerHTML);
+    });
 	$('#aione_header').load('template/aione_header.html', function() {
 		var options = {
 			valueNames: ['aione-search-item'],
