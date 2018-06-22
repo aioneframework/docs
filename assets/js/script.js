2213170bf5268dca51ec7c6a071fe932346965d5
sgssandhu <sgs.sandhu@gmail.com>
2018-06-22 10:41:32 +0530

    Update 20180622051132

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,34 @@
 $(document).ready(function() {
-	
-    $(".options").click(function(e) {
-        var options = "";
+	load_options();
+	$(document).on('click', '.options', function(e) {
+		load_options();
+		//init_sliders();
+	});
+	$(document).on('change', '.options input, .options select', function(e) {
+		load_options();
+		//init_sliders(); 
+	});
+
+	function load_options(){
+		var classes = [];
+		var data = [];
         var options_data = $(this).find("form").serializeArray();
+        console.log("options_data");
+        	console.log(options_data);
         jQuery.each( options_data, function( index, field ) {
-          options += " " + field.value;
+        	console.log("Index"+index);
+        	console.log("field");
+        	console.log(field);
+        	if(false){
+
+        	} else{
+        	}
+          		classes.push(field.value);
         });
-        $( "#tab_preview > *").attr('class',options);
+        classes.join(" ")
+        $( "#tab_preview > *").attr('class',classes);
         $( "#tab_code > textarea").val($( "#tab_preview > *")[0].outerHTML);
-    });
-    $(".options").click();
+	}
 	$('#aione_header').load('template/aione_header.html', function() {
 		var options = {
 			valueNames: ['aione-search-item'],
@@ -18,6 +37,4 @@ $(document).ready(function() {
 		};
 		var search = new List('aione_nav', options);
 	});
-	//$('#aione_footer').load('template/footer.html');
-	//$('#aione_copyright').load('template/copyright.html');
 });
